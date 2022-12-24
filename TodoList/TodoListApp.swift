fbad71df0a120752f790df91c10264e33a00d991
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-24 17:17:33 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Philipp Sanktjohanser on 24.12.22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
